d715c8f3d352b7cb88c238340a2fdd7de15cf65d
Carla Vega Jarquin <4vegac44@solent.ac.uk>
2020-03-05 12:56:53 +0000

    SUM BUTTON

--- 6-js/script.js
+++ 6-js/script.js
@@ -25,6 +25,17 @@ document.getElementById("form2-submit").onclick = function(){
     handleForm2();
 }
 
+function suma(){
+    var firstNumber = document.getElementById("num1").value;
+    var secondNumber = document.getElementById("num2").value;
+    var sum = firstNumber + secondNumber;
+    alert("The sum is " + sum);
+}
+
+document.getElementById("sumnum").onclick = function(){
+    suma();
+}
+
 function sum2(){
     alert(sum(3, 5));
 }
