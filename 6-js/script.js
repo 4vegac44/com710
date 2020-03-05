44d3d06beecfd1849aebb69d5b97b65235c3a15a
Carla Vega Jarquin <4vegac44@solent.ac.uk>
2020-03-05 12:05:23 +0000

    Created JavaScript functions

--- 6-js/script.js
+++ 6-js/script.js
@@ -1,1 +1,23 @@
 
+function handleForm1() {
+    // display a pop-up dialog box with an answer
+    alert("Hello");
+}
+
+document.getElementById('form1-submit').onclick = function() {
+    handleForm1();
+};
+
+function sum(firstNumber, secondNumber) {
+    var message = "The sum is " + (firstNumber + secondNumber);
+    return message
+}
+
+function sum2(){
+    alert(sum(3, 5));
+}
+
+document.getElementById('button2').onclick = function() {
+    sum2();
+};
+
