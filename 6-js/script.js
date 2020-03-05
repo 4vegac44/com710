02589cc27b0fc9bd606f0f80dbeb348213b9f49f
Carla Vega Jarquin <4vegac44@solent.ac.uk>
2020-03-05 12:35:20 +0000

    More js functions

--- 6-js/script.js
+++ 6-js/script.js
@@ -1,3 +1,6 @@
+function changeColor(btn) {
+    btn.style.backgroundColor = "green";
+  }
 
 function handleForm1() {
     // display a pop-up dialog box with an answer
@@ -11,6 +14,15 @@ document.getElementById('form1-submit').onclick = function() {
 function sum(firstNumber, secondNumber) {
     var message = "The sum is " + (firstNumber + secondNumber);
     return message
+}
+
+function handleForm2(){
+    var text = document.getElementById("form2-text").value;
+    alert(text);
+}
+
+document.getElementById("form2-submit").onclick = function(){
+    handleForm2();
 }
 
 function sum2(){
